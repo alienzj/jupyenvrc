24358535332435f7bc6f3f9bad0dbb4e55b792d9
Jie Zhu <alienchuj@gmail.com>
2024-09-26 11:59:05 +0800

    bioenv with flake.nix directly

--- flake.nix
+++ flake.nix
@@ -1,43 +1,53 @@
 {
-  description = "Your jupyenv project";
+  description = "A Reproducible Computing Environment with Jupyter and NixOS";
 
-  nixConfig.extra-substituters = [
-    "https://tweag-jupyter.cachix.org"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
-  ];
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
 
-  inputs.flake-compat.url = "github:edolstra/flake-compat";
-  inputs.flake-compat.flake = false;
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.jupyenv.url = "github:tweag/jupyenv";
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" ];
+    eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
+      inherit system;
+      pkgs = import nixpkgs { inherit system; };
+    });
+  in
+  {
+    # use them via `nix develop .#xxx` or `direnv allow`
+    devShells = eachSystem ({pkgs, ...}: rec {
+      default = bioenv;
+      bioenv = pkgs.mkShell {
+        packages = with pkgs; [
+          python312
+	  python312Packages.jupyterlab
+	  python312Packages.pandas
+	  python312Packages.numpy
+	  python312Packages.biopython
+        ];
 
-  outputs = {
-    self,
-    flake-compat,
-    flake-utils,
-    nixpkgs,
-    jupyenv,
-    ...
-  } @ inputs:
-    flake-utils.lib.eachSystem
-    [
-      flake-utils.lib.system.x86_64-linux
-    ]
-    (
-      system: let
-        inherit (jupyenv.lib.${system}) mkJupyterlabNew;
-        jupyterlab = mkJupyterlabNew ({...}: {
-          nixpkgs = inputs.nixpkgs;
-          imports = [(import ./kernels.nix)];
-        });
-      in rec {
-        packages = {inherit jupyterlab;};
-        packages.default = jupyterlab;
-        apps.default.program = "${jupyterlab}/bin/jupyter-lab";
-        apps.default.type = "app";
-      }
-    );
+	#packages = [
+        #  (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+        #  # select Python packages here
+        #    pandas
+        #    requests
+        #  ]))
+        #];
+
+      };
+    });
+
+    # use them via `nix run .#xxx`
+    apps = eachSystem ({system, pkgs, ...}: rec {
+      default = jupyter;
+
+      jupyter = {
+        # TODO: change this script to your own app entry
+        type = "app";
+        program = "${pkgs.writeShellScript "jupyter-lab" ''
+          jupyter lab ./workspace
+        ''}";
+      };
+    });
+  };
 }
